2c69c2c0c243e34c2d6b7fc4c2464a7156ecdb79
Evgeniy <leafmind@gmail.com>
2016-12-13 16:29:16 +0400

    remove regex from config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   config.action_cable.url = Settings.heroku.action_cable.url
-  config.action_cable.allowed_request_origins = Settings.heroku.action_cable.allowed_request_origins
+  config.action_cable.allowed_request_origins = [Settings.heroku.action_cable.allowed_request_origins.url, Regex.new(Settings.heroku.action_cable.allowed_request_origins.regex)]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
