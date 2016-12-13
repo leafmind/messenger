cef92aae1f30d31093340722990b57996b1b1d85
Evgeniy <leafmind@gmail.com>
2016-12-13 14:02:55 +0400

    add cable urls according to heroku rules

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.url = 'wss://ancient-shelf-98201.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://ancient-shelf-98201.herokuapp.com', /http:\/\/ancient-shelf-98201.herokuapp.com.*/ ]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
